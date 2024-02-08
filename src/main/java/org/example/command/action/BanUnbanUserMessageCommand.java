90e6d742b64ad9474a7e65c907dd7c5826650e31
Coxeyyy <artemcxy@gmail.com>
2024-02-08 21:25:59 +0300

    Ban/Unban недоступен для обычных пользователей

--- src/main/java/org/example/command/action/BanUnbanUserMessageCommand.java
+++ src/main/java/org/example/command/action/BanUnbanUserMessageCommand.java
@@ -3,19 +3,28 @@ package org.example.command.action;
 import org.example.command.Command;
 import org.example.data.DataBase;
 import org.example.data.User;
+import org.example.data.UserType;
+import org.example.result.ForwardResult;
 import org.example.result.RedirectResult;
 import org.example.result.Result;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.util.Optional;
 
 import static org.example.Resources.COMMAND_SHOW_ADMIN_PANEL;
+import static org.example.Resources.PAGE_CHAT;
 
 public class BanUnbanUserMessageCommand implements Command {
     @Override
     public Result execute(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        User userSession = (User) session.getAttribute("user");
+        if (userSession.getUserType().equals(UserType.CLIENT)) {
+            return new ForwardResult(PAGE_CHAT);
+        }
         String loginInput = request.getParameter("loginInput");
         Optional<User> user = DataBase.findUserByLogin(loginInput);
         if (user.isPresent() && user.get().isWriteMessages()) {
